37af56f739111b06441a0dbd6a1b50a742ad931e
etarise <73929569+etarise@users.noreply.github.com>
2022-07-22 10:09:42 +0200

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApp.Models
+{
+    public class Person  // class person object that is hold the atrributes of a  person object
+    {
+      
+        public string  FirstName { get; set; }  //  claas attribute firstname
+        public string LastName { get; set; } //  claas attribute lastname
+    }
+}
---
+++ PersonController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using TestWebApp.Models;
+
+namespace TestWebApp.Controllers
+{
+    public class PersonController : Controller  // person controller containing application logic
+    {
+        [HttpGet]
+        public IActionResult Index() //get action takes user to the form to capture details
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Person person) // post action result receives object person from the view
+        {
+            // file and file path  declaration
+            string TextFile = "Data.json";
+            string path = @"c:\\MyWebTest\\";
+            string Filepath = path + TextFile;
+
+            //converting the posted person object data to json using the JsonConvert.SerializeObject  method from the json namespace
+            string json = JsonConvert.SerializeObject("First Name ===> "+ person.FirstName +"  "+ "Last Name ===> "+ person.LastName);
+
+           
+           
+            // create path to write our json object  if it does not exist 
+            bool exists = System.IO.Directory.Exists(path);
+            if (!exists)
+            {
+                System.IO.Directory.CreateDirectory(path);
+
+            }else
+            {
+               // write our json object to file 
+                System.IO.File.WriteAllText(Filepath, json);
+            }
+
+            return View();
+        }
+
+    }
+}
